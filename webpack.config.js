eb1ff01e8fbb2ecc0ef317535190c201ccc2447b
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-06-19 20:27:54 +0300

    feat: run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -7,6 +8,47 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
+        filename: '[name][contenthash].js',
+        clean: true,
     },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname,'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ]
 }
