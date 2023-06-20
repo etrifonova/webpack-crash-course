e5102d0eaa969819bac844851c8170fcc5fd999e
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-06-20 11:01:31 +0300

    feat: npm run build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import generateJoke from "./generateJoke";
 import './styles/main.scss';
+import smiley from './assets/smiley.svg';
+
+const smileImg = document.getElementById('smileImg');
+smileImg.src = smiley;
 
 console.log(generateJoke());
